900b6a54ef111ffa0c6ddf6058241afef1b33f18
oscar@DESKTOP-E3VE1D8 <4abcgarciaoscar@gmail.com>
2023-02-01 17:42:04 +0100

    lcd screen code added

--- prueba_conexion/prueba_conexion.ino
+++ prueba_conexion/prueba_conexion.ino
@@ -1,17 +1,14 @@
 #include <SPI.h>
 #include <Ethernet.h>
-
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2,3,4,5,6,7);
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };//Ponemos la dirección MAC de la Ethernet Shield
 IPAddress ip(192,168,1,177); //Asignamos  la IP al Arduino
 EthernetServer server(80); //Creamos un servidor Web con el puerto 80 que es el puerto HTTP por defecto
  
-int LED1=2; //Pin del LED 1
-int LED2=3; //Pin del LED 2
-String estado1="OFF"; //Estado del Led 1 inicialmente "OFF"
-String estado2="OFF"; //Estado del Led 2 inicialmente "OFF" 
-
 void setup()
 {
+  lcd.begin(16, 2);
   Serial.begin(9600);
  
   // Inicializamos la comunicación Ethernet y el servidor
@@ -21,12 +18,6 @@ void setup()
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
   
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(4,INPUT);
-  pinMode(5,INPUT);
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED2,HIGH);
 }
  
 void loop()
@@ -47,67 +38,25 @@ void loop()
            
                    //Ya que hemos convertido la petición HTTP a una cadena de caracteres, ahora podremos buscar partes del texto.
                    int posicion=cadena.indexOf("Data="); //Guardamos la posición de la Palabra "Data=" a la variable 'posicion'
-           
-                    if(cadena.substring(posicion)=="Data=1")//Si en la posición hay "Data=1"
+                   int endPosition =  cadena.indexOf(";");
+                   String data = cadena.substring((posicion+5),endPosition);
+                   int longi = cadena.length();
+                   cadena.replace("%20"," ");
+                    if(posicion>=0)//Si en la posición hay "Data=1"
                     {
-                      digitalWrite(LED1,LOW);
-                      estado1="ON";
-                    }
-                    else if(cadena.substring(posicion)=="Data=2")//Si en posición hay "Data=2"
-                    {
-                      digitalWrite(LED1,HIGH);
-                      estado1="OFF";
-                    }
-                    else if(cadena.substring(posicion)=="Data=3")//Si en la posición hay "Data=3"
-                    {
-                      digitalWrite(LED2,LOW);
-                      estado2="ON";
-                    }
-                    else if(cadena.substring(posicion)=="Data=4")//Si en la posición hay "Data=4"
-                    {
-                      digitalWrite(LED2,HIGH);
-                      estado2="OFF";
-                    } 
-                          
+                      if () {
+                      statements
+                      }
+                      lcd.setCursor(0, 0);
+                      lcd.print();
+                    }    
         }
         //Cuando reciba una línea en blanco, quiere decir que la petición HTTP ha acabado y el servidor Web está listo para enviar una respuesta
         if (c == 'n' && currentLineIsBlank) {
  
             // Enviamos al cliente una respuesta HTTP
             client.println("HTTP/1.1 200 OK");
-            client.println("Content-Type: text/html");
-            client.println();
- 
-            //Página web en formato HTML
-            client.println("<html>");                 
-            client.println("<head><title>Naylamp Mechatronics</title>");
-            client.println("</head>");
-            client.println("<body>");
-            client.println("<div style='text-align:center;'>");
-            client.println("<h1>NAYLAMP MECHATRONICS</h1>");
-            client.println("<h2>Entradas Analogicas</h2>");
-            client.print("AN0="); client.println(analogRead(0));
-            client.print("<br />AN1=");client.println(analogRead(1)); 
-            client.println("<h2>Entradas Digitales</h2>");
-            client.print("D4=");client.println(digitalRead(4));
-            client.println("<br />D5=");client.print(digitalRead(5));
-            client.println("<br /><br />");
-            client.println("<a href='http://192.168.1.177'>Actualizar entradas</a>");
-            client.println("<h2>Salidas Digitales </h2>");        
-            client.println("Estado del LED 1 = ");client.print(estado1);            
-            client.println("<br />");            
-            client.print("<button onClick=location.href='./?Data=1'>ON</button>");           
-            client.print("<button onClick=location.href='./?Data=2'>OFF</button>");
-            client.println("<br /><br />");
-            client.println("Estado del LED 2 = ");client.print(estado2);            
-            client.println("<br />");            
-            client.print("<button onClick=location.href='./?Data=3'>ON</button>");           
-            client.print("<button onClick=location.href='./?Data=4'>OFF</button>");
-            client.println("<br /><br />");
-            client.println("<a href='https://naylampmechatronics.com/'>naylampmechatronics.com</a>");
-            client.println("<br /><br />");             
-            client.println("</b></body>");
-            client.println("</html>");
+            client.println("<html>Conexión realizada con exito</html>");
             break;
         }
         if (c == 'n') {
